b8da63fdc5c0df7488780a2b005d48e18346d297
morris <morristwinomugisha2000@gmail.com>
2025-01-10 12:50:39 +0300

    fix: use connection string

--- db/pool.js
+++ db/pool.js
@@ -1,21 +1,10 @@
 const { Pool } = require('pg');
 
-// Only use dotenv in development
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || {
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
-  },
-  ssl: process.env.NODE_ENV === 'production' ? {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
     rejectUnauthorized: false
-  } : false
+  }
 });
 
 module.exports = pool;
