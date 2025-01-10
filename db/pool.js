827d759507e7641e42aea8770a1d664b7905b52f
morris <morristwinomugisha2000@gmail.com>
2025-01-10 17:43:38 +0300

    fix update pool

--- db/pool.js
+++ db/pool.js
@@ -2,16 +2,11 @@ const { Pool } = require('pg');
 
 require("dotenv").config();
 
-const pool = new Pool({
-  host: process.env.PGHOST ,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-module.exports = pool;
+module.exports = new Pool({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+})
 
